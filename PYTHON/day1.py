8823fd258d5f4b85c6aa98a3bf60f13140776360
Gudapur Koushik <tgznotaxid@gmail.com>
2026-06-10 21:19:46 +0530

    Started python

---
+++ PYTHON/day1.py
@@ -0,0 +1,32 @@
+"""  print('Twinkle Twinkle little star')"""
+
+import os
+#print(dir)
+
+a = 30
+b= "koushik"
+c = 75.4
+#type()
+
+type(a)
+
+#typecasting
+
+print(int(c))
+
+#input
+
+A = input("Enter the name: \n")
+
+# practice
+
+x = int(input("Enter A"))
+y = int(input("Enter B"))
+if(x<y): print("false")
+print("Sum of two numbers:",x+y)
+print("Average of two numbers:",(x+y)/2)
+print("Square of numbers:",x*x," ",y*y)
+
+# reminder
+
+print("remainder",x%y);
