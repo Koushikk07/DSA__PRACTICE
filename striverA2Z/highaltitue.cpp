45f70c39294ceae31a5b2dc7dcf41e49247e7faf
Gudapur Koushik <tgznotaxid@gmail.com>
2026-06-19 20:28:57 +0530

    random leetcode

---
+++ striverA2Z/highaltitue.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int largestAltitude(vector<int> &gain)
+{
+    int n = gain.size();
+    int mx = 0;
+
+    for (int i = 0; i <= n; i++)
+    {
+        int alt = 0;
+
+        for (int j = 0; j < i; j++)
+        {
+            alt += gain[j];
+        }
+
+        mx = max(mx, alt);
+    }
+
+    return mx;
+}
