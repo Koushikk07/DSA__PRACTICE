52f19532ae5021ac632911656905e7e9029aee92
Gudapur Koushik <tgznotaxid@gmail.com>
2026-06-23 17:42:23 +0530

    Easy Leetcode problems

---
+++ uglynum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        for (int i = 2; i < 6 && n; i++)
+            while (n % i == 0)
+                n /= i;
+        return n == 1;
+    }
+};
+
+int main()
+{
+}
