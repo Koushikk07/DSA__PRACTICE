ee019acd0c92aba13aa950b624e5c567c06bb645
Gudapur Koushik <tgznotaxid@gmail.com>
2026-07-18 19:26:56 +0530

    LL med problems + Leetcode POTD

---
+++ striverA2Z/gcd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findGCD(vector<int> &nums)
+    {
+
+        sort(nums.begin(), nums.end());
+        return gcd(nums[0], nums[nums.size() - 1]);
+    }
+};
