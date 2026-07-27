37f68caa15b055b441e2dc7aea21e81f3479d12a
Gudapur Koushik <tgznotaxid@gmail.com>
2026-07-27 06:55:44 +0530

    modified

---
+++ PRACTICE/maxProdtwo.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int a = 0, b = 0;
+        for (int x : nums) {
+            if (x >= a) {
+                b = a;
+                a = x;
+            } else if (x > b) {
+                b = x;
+            }
+        }
+        return (a - 1) * (b - 1);
+    }
+};
