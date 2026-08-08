c5ae7e13127e3f8ef15dcbd4bb9d07dfcfe5a1ab
Gudapur Koushik <tgznotaxid@gmail.com>
2026-08-08 21:02:56 +0530

    python started - basics

--- PYTHON/basics.py
+++ PYTHON/basics.py
@@ -7,17 +7,35 @@ joke = pyjokes.get_joke()
 
 # OS ---> os. .. directories
 
-a = 30
+""" a = 30
 b = "Koushik"
 c= 71.22
 
-""" print(type(a))
+print(type(a))
 print(type(b))
-print(type(c)) """
+print(type(c)) 
 
-''' Type casting'''
+Type casting
 print(str(a))
 print(int("32"))
 print(float(32))
 
-A = int(input("enter the value"))
+A = int(input("enter the value")) """
+
+""" name = (input("Enter your name:\n"))
+
+print("Hi! "+name+", Welcome to World.");
+
+for i in range (0,10):
+    print(i,name) """
+""" i=0
+while(i!=10):
+    print(f"{i} koushik bhai bolte !")
+    i=i+1 """
+    
+def sum(a,b):
+    return a+b
+
+a=int(input("Enter the num1:"))
+b=int(input("Enter the num2:"))
+print("Sum of Two numbers:",sum(a,b))
