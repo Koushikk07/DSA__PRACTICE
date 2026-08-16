6c9fec524bf21b94670573c1c1cb347986506248
Gudapur Koushik <tgznotaxid@gmail.com>
2026-08-16 09:47:01 +0530

    String east problem

---
+++ striverA2Z/lengthSub.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximumLengthSubstring(string s)
+    {
+        unordered_map<char, int> mp;
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.size(); right++)
+        {
+            mp[s[right]]++;
+
+            while (mp[s[right]] > 2)
+            {
+                mp[s[left]]--;
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
