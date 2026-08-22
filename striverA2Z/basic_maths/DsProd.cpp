5a12030fa2449b11da4a4433185f3e028b407e4c
Gudapur Koushik <tgznotaxid@gmail.com>
2026-08-22 22:08:59 +0530

    Leetcode POTD easy

---
+++ striverA2Z/basic_maths/DsProd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool checkDivisibility(int n)
+    {
+
+        int original = n;
+        int sum = 0;
+        int product = 1;
+
+        while (n > 0)
+        {
+            int digit = n % 10;
+
+            sum += digit;
+            product *= digit;
+
+            n /= 10;
+        }
+
+        return original % (sum + product) == 0;
+    }
+};
