87e98b4de865ca409edf01f4ccbd62eb7d560b4e
Gudapur Koushik <tgznotaxid@gmail.com>
2026-08-27 22:04:42 +0530

    Basic Math Leetcode problems

---
+++ striverA2Z/basic_maths/FourDig.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minimumSum(int num)
+    {
+        vector<int> temp;
+        while (num > 0)
+        {
+            int d = num % 10;
+            temp.push_back(d);
+            num = num / 10;
+        }
+        sort(temp.begin(), temp.end());
+        int x = temp[0] * 10 + temp[3];
+        int y = temp[1] * 10 + temp[2];
+        return x + y;
+    }
+};
