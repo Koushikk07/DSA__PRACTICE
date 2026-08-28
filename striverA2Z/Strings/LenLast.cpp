0333055ee111601ab123e8690663d8ea83343d36
Gudapur Koushik <tgznotaxid@gmail.com>
2026-08-28 22:11:41 +0530

    String basic leetcode problem

---
+++ striverA2Z/Strings/LenLast.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int length = 0;
+        bool cnt = false;
+
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+
+            if (s[i] != ' ')
+            {
+                cnt = true;
+                length++;
+            }
+            else if (cnt)
+            {
+
+                break;
+            }
+        }
+        return length;
+    }
+};
