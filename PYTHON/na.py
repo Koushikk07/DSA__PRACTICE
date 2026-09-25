fc3298e788610ccadfa84be3e9b0638474239f04
Gudapur Koushik <tgznotaxid@gmail.com>
2026-09-25 21:18:12 +0530

    python recap

---
+++ PYTHON/na.py
@@ -0,0 +1,44 @@
+import string
+
+'''name=input("enter your name:")
+i=1
+while i<10:
+    i+=1
+    print(i," ",name)'''
+
+
+""" a=int(input("enter the value of A :"))
+b=int(input("enter the value of B :"))
+operation = input("Enter the operation(+,-,%,/,*):")
+if operation=="+":
+    print(a+b)
+
+elif operation=="-":
+    print(a-b)
+elif operation=="*":
+    print(a*b)
+elif operation=="/":
+    print(a/b)
+else:
+    print("Wrong operation!") """
+
+""" for i in range(1,11):
+    if(i==8): break
+    print(i); """
+
+""" arr = [1,2,3,3,4,4,5,46,467,467]
+
+for i in arr:
+    print()
+
+    """
+
+name = input("enter your name:")
+
+i=len(name)-1
+while(i>=0):
+    print(name[i])
+    i-=1
+
+
+
---
+++ PYTHON/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+for i in range(1,11):
+    if(i==8): break
+    print(i);
